eac8ed46fbbfd56fb743c3a4e26d0fd127a1c0a3
Abir Santra <santraabir8@gmail.com>
2022-07-25 10:05:49 +0530

    Changed Howitworks background container

--- src/components/Howitworks.jsx
+++ src/components/Howitworks.jsx
@@ -4,11 +4,11 @@ import RightArrow from "../img/rightarrow.svg";
 
 const Howitworks = () => {
   return (
-    <div
-      className="works section bg-no-repeat bg-cover bg-center"
-      style={{ backgroundImage: "url('Wave.svg')" }}
-    >
-      <div className="section-container py-24 flex justify-center items-center flex-col gap-16">
+    <div className="works section ">
+      <div
+        className="section-container py-24 flex justify-center items-center flex-col gap-16 bg-no-repeat bg-cover bg-center rounded-3xl"
+        style={{ backgroundImage: "url('Wave.svg')" }}
+      >
         <div className="section-header flex justify-center items-center flex-col gap-2">
           <h2 className="header-subtitle text-main-light">How it works</h2>
           <h1 className="header-title text-white-2">So how does it work?</h1>
